6d81ddc1cff859b13dbe404dd33c6849c9254b73
madhaviporte <madhaviporte2004@gmail.com>
2026-02-14 16:48:40 +0530

    day-10

---
+++ Day-10/Q2.js
@@ -0,0 +1,7 @@
+// print star 
+let prompt = require('prompt-sync')()
+let n = Number(prompt("Enter a start"))
+
+for( let i=1; i<=n; i++){
+    process.stdout.write("* ")
+}
---
+++ Day-10/Q5.js
@@ -0,0 +1,12 @@
+//inverted right angle triangle
+
+let prompt = require('prompt-sync')()
+let n = Number(prompt('Enter a star'))
+
+for(let i=0; i<=n; i++){
+    for(let j=0; j<=n-i; j++){
+        process.stdout.write("* ")
+    }
+    console.log();
+    
+}
